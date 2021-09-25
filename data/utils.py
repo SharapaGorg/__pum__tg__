485163bc8a2143db38ed3c26222a0c1e7a4b1f6c
SharapaGorg <sharapov.savely@yandex.ru>
2021-09-25 15:14:06 +0300

    teacher schedule

--- data/utils.py
+++ data/utils.py
@@ -1,54 +1,61 @@
-from threading import current_thread
-import xlrd, sys, os
+import xlrd
+import sys
+import os
 
-if sys.platform == "win32" : spliter = "\\"
-else: spliter = "/"
+if sys.platform == "win32":
+    spliter = "\\"
+else:
+    spliter = "/"
 
-try: from constants import bells_8_9, bells_10_11, data_file
-except: from data.constants import bells_8_9, bells_10_11, data_file
+try:
+    from constants import bells_8_9, bells_10_11, assets_folder, static_folder
+except:
+    from data.constants import bells_8_9, bells_10_11, assets_folder, static_folder
 
 if __name__ == "__main__":
     folder = "assets" + spliter
 else:
     folder = "data" + spliter + "assets" + spliter
 
-window_counter = 0
 temp_group = str()
+
 
 class Day:
     def __init__(
         self,
-        title : str
+        title: str
     ):
 
         self.title = title
-        self.shedule = list()
+        self.schedule = list()
 
-    def update_shedule(self, lesson):
-        self.shedule.append(lesson)
+    def update_schedule(self, lesson):
+        self.schedule.append(lesson)
 
     def __str__(self):
         return self.title
 
+
 class Week:
     def __init__(
-        self):
+            self):
 
-        self.shedule = list()
+        self.schedule = list()
 
-    def update_shedule(self, lesson):
-        self.shedule.append(lesson)
+    def update_schedule(self, lesson):
+        self.schedule.append(lesson)
 
     def __str__(self):
-        return '<Week shedule>'
+        return '<Week schedule>'
+
 
 class Lesson:
-    def __init__(self, 
-                title : str, 
-                group : str, 
-                teacher : str, 
-                cab : str, 
-                bell = '00:00'):
+    def __init__(self,
+                 title: str = 'Окно',
+                 group: str = '',
+                 teacher: str = '?',
+                 cab: str = '000',
+                 bell='00:00'):
 
         self.title = title
         self.group = group
@@ -59,16 +66,17 @@ class Lesson:
     def __str__(self):
         return self.title
 
-class StudentShedule:
-    def __init__(self, name : str):
+
+class StudentSchedule:
+    def __init__(self, name: str):
         self.name = name
-        self.shedule = dict()
+        self.schedule = dict()
         self.window_checker = 0
 
         __week = Week()
         __day = None
 
-        SOURCE = 'data' + spliter + data_file
+        SOURCE = 'data' + spliter + assets_folder
 
         is_found = False
         for student in os.listdir(SOURCE):
@@ -76,45 +84,48 @@ class StudentShedule:
                 self.name = student.rstrip('.xlsx')
                 is_found = True
 
-                data =  xlrd.open_workbook(SOURCE + spliter + student).sheet_by_index(0)
+                data = xlrd.open_workbook(
+                    SOURCE + spliter + student).sheet_by_index(0)
 
                 counter = 0
                 for i in range(data.nrows):
-                    if counter == 14: 
+                    if counter == 14:
                         counter = 0
 
                     first_col = data.row_values(i)[0]
-                    second_col = data.row_values(i)[1] # subject title
+                    second_col = data.row_values(i)[1]  # subject title
                     third_col = data.row_values(i)[2]  # group title
-                    fourth_col = data.row_values(i)[3] # teacher name
+                    fourth_col = data.row_values(i)[3]  # teacher name
                     fifth_col = data.row_values(i)[4]  # cabinet number
 
                     if "8" in third_col or "9" in third_col or "10" in third_col or "11" in third_col:
                         temp_group = third_col.split('-')[1]
 
                     if counter == 0:
-                        if __day : 
-                            __week.update_shedule(__day)
+                        if __day:
+                            __week.update_schedule(__day)
 
                         __day = Day(first_col)
-                    if counter == 1: pass
-                    else: 
-                        _lesson = Lesson(second_col, third_col, fourth_col, fifth_col)
-                        __day.update_shedule(_lesson)
+                    if counter == 1:
+                        pass
+                    else:
+                        _lesson = Lesson(second_col, third_col,
+                                         fourth_col, fifth_col)
+                        __day.update_schedule(_lesson)
 
                     counter += 1
 
-                __week.update_shedule(__day)
+                __week.update_schedule(__day)
 
-                for day in __week.shedule:
-                    (day.shedule).remove(day.shedule[0])
+                for day in __week.schedule:
+                    (day.schedule).remove(day.schedule[0])
 
                     checker = False
-                    for i in range(len(day.shedule) - 1, 0, -1):
-                        lesson = day.shedule[i]
+                    for i in range(len(day.schedule) - 1, 0, -1):
+                        lesson = day.schedule[i]
 
                         if (len(lesson.title) == 0 and not checker):
-                            day.shedule.remove(lesson)
+                            day.schedule.remove(lesson)
 
                         if (len(lesson.title) == 0 and checker):
                             self.window_checker += 1
@@ -122,19 +133,125 @@ class StudentShedule:
                         if (len(lesson.title) > 0):
                             checker = True
 
-                    for i in range(len(day.shedule)):
+                    for i in range(len(day.schedule)):
                         if temp_group == '9' or temp_group == '8':
-                            day.shedule[i].bell = bells_8_9[i]
+                            day.schedule[i].bell = bells_8_9[i]
 
                         if temp_group == '10' or temp_group == '11':
-                            day.shedule[i].bell = bells_10_11[i]
-                            
-                self.shedule = __week.shedule
+                            day.schedule[i].bell = bells_10_11[i]
 
-                break      
+                self.schedule = __week.schedule
+
+                break
 
         if not is_found:
             raise Exception('Student not found')
 
     def __str__(self):
         return self.name
+
+
+class Teacher:
+    __title__ = 'Teacher'
+    __folder__ = 'data' + spliter + assets_folder
+
+    __TEACHERS = dict()
+    _schedule = None
+
+    def __init__(self, name, *args):
+        self._name = name
+        self.args = args
+
+    def __update(self):
+        """
+        Update teacher`s schedule and fill with new information
+        base on all students schedule
+        """
+
+        for student in os.listdir(self.__folder__):
+            student_schedule = xlrd.open_workbook(
+                self.__folder__ + spliter + student).sheet_by_index(0)
+
+            day_title = str()
+
+            for i in range(student_schedule.nrows):
+
+                first_column = student_schedule.row_values(
+                    i)[0]  # number of lesson or day title
+                subject_title = student_schedule.row_values(i)[1]
+                group_title = student_schedule.row_values(i)[2]
+                teacher_name = student_schedule.row_values(i)[3]
+                cabinet_number = student_schedule.row_values(i)[4]
+
+                if len(first_column) in range(1, 3):
+                    current_lesson = Lesson(
+                        subject_title, group_title, teacher_name, cabinet_number)
+
+                    if '8' in group_title or '9' in group_title:
+                        current_lesson.bell = bells_8_9[int(first_column) - 1]
+                    if '10' in group_title or '11' in group_title:
+                        current_lesson.bell = bells_10_11[int(first_column) - 1]
+
+                    if teacher_name not in self.__TEACHERS.keys():
+                        # 8 - maximum of lessons county
+                        self.__TEACHERS[teacher_name] = {
+                            'Понедельник' : None,
+                            'Вторник' : None,
+                            'Среда' : None,
+                            'Четверг' : None,
+                            'Пятница' : None,
+                            'Суббота' : None
+                        }
+
+                    if not self.__TEACHERS[teacher_name][day_title]:
+                        self.__TEACHERS[teacher_name][day_title] = Day(
+                            day_title)
+                        self.__TEACHERS[teacher_name][day_title].schedule = [
+                            Lesson() for i in range(10)]
+
+                    self.__TEACHERS[teacher_name][day_title].schedule[int(
+                        first_column) - 1] = current_lesson
+
+                elif len(first_column) != 0:
+                    day_title = first_column
+
+    @property
+    def name(self):
+        self.__update()
+
+        for teacher_name in self.__TEACHERS:
+            if self._name in teacher_name:
+                self._name = teacher_name
+                self._schedule = self.__TEACHERS[self._name]
+
+        return self._name
+
+    @property
+    def schedule(self):
+        self.name
+        will_delete = list()
+
+        for day_title in self._schedule:
+            day = self._schedule[day_title]
+
+            if not day:
+                will_delete.append(day_title)
+                continue
+
+            checker = False
+            for i in range(len(day.schedule) - 1, 0, -1):
+                lesson = day.schedule[i]
+
+                if (lesson.title == 'Окно' and not checker):
+                    day.schedule.remove(lesson)
+
+                if (lesson.title != 'Окно'):
+                    checker = True
+
+        for day_title in will_delete:
+            self._schedule.pop(day_title)
+
+        return self._schedule
+
+    def __str__(self):
+        return self.__title__
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import os, sys
+import os
+import sys
 
 from data.config import settings
 from data import LogPython
-from data.utils import StudentShedule
+from data.utils import StudentSchedule, Teacher
 
-if sys.platform == "win32" : spliter = "\\"
-else: spliter = "/"
+if sys.platform == "win32":
+    spliter = "\\"
+else:
+    spliter = "/"
 
 try:
     from aiogram import Bot, types
@@ -16,62 +19,108 @@ try:
     from aiogram.utils import executor
 except:
     os.system(f"python3 data{spliter}deps.py")
-        
+
     sys.exit(0)
 
-SHEDULE_FILE = "data" + spliter + "SHEDULE_DATA.json"
-
-bot = Bot(token = settings['TOKEN'])
+bot = Bot(token=settings['TOKEN'])
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-class SheduleDataInput(StatesGroup):
+
+class ScheduleDataInput(StatesGroup):
     r = State()
-    
+
+
 class FindingLessonDataInput(StatesGroup):
     r = State()
 
-@dp.message_handler(commands = ['start'])
-async def procces_start_command(message : types.Message):
-    await message.reply("Салам алейкум")
+
+class TeacherScheduleDataInput(StatesGroup):
+    r = State()
+
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await bot.send_message(message.from_user.id, 'start - greeting')
 
-@dp.message_handler(commands = ['shedule'])
-async def proccess_shedule_command(msg : types.Message):
-    await bot.send_message(msg.chat.id,'Напишите, кто Вы, в формате Фамилия Имя Отчество')
-    await SheduleDataInput.r.set()
-    
-@dp.message_handler(commands = ['find_victim'])
-async def proccess_find_victim_command(msg : types.Message):
+
+@dp.message_handler(commands=['schedule'])
+async def proccess_schedule_command(msg: types.Message):
+    await bot.send_message(msg.chat.id, 'Напишите, кто Вы, в формате Фамилия Имя Отчество')
+    await ScheduleDataInput.r.set()
+
+
+@dp.message_handler(commands=['teacher_schedule'])
+async def proccess_teacher_schedule_command(msg: types.Message):
+    await bot.send_message(msg.chat.id, 'Напишите фамилию преподавателя')
+    await TeacherScheduleDataInput.r.set()
+
+
+@dp.message_handler(commands=['find_victim'])
+async def proccess_find_victim_command(msg: types.Message):
     await bot.send_message(msg.chat.id, "Напишите запрос в формате день_недели урок урок_по_счёту\nНапример:\nчеТвеРГ инфорМАтика 1\nвтОрник оКНо 2")
     await FindingLessonDataInput.r.set()
 
-@dp.message_handler(state = SheduleDataInput.r)
-async def shedule(msg : types.Message, state : FSMContext):
+
+@dp.message_handler(state=TeacherScheduleDataInput.r)
+async def teacher_schedule(msg: types.Message, state: FSMContext):
+    r = msg.text
+
+    groups = set()
+    teacher = Teacher(r)
+
+    message = teacher.name + '\n\nДень недели:\nВремя начала - кабинет Название Группа\n'
+
+    for day_title in teacher.schedule:
+        day = teacher.schedule[day_title]
+        message += '\n' + day_title + ':\n'
+
+        for lesson in day.schedule:
+            spacer, temp_cab = 0, ''
+
+            if len(lesson.cab) < 3:
+                temp_cab = "000"
+            elif len(lesson.cab) > 3:
+                spacer = 4
+
+            try:
+                message += lesson.bell + " - " + temp_cab + lesson.cab + ' ' * \
+                    (16 - len(lesson.cab) - len(temp_cab) - spacer) + \
+                    str(lesson) + ' ' + lesson.group + '\n'
+            except:
+                pass
+
+            groups.add(lesson.group)
+
+    await bot.send_message(msg.from_user.id, message)
+    await state.finish()
+
+
+@dp.message_handler(state=ScheduleDataInput.r)
+async def schedule(msg: types.Message, state: FSMContext):
     r = msg.text
     try:
-        data = StudentShedule(r)
-        
+        data = StudentSchedule(r)
+
         res = data.name + ":\n\nДень недели:\nВремя начала - кабинет Название\n\n"
         res += f'Количество окон --> `{data.window_checker}`\n'
 
         groups = set()
 
-        for day in data.shedule:
-            res += "\n" + day.title + ":\n" 
+        for day in data.schedule:
+            res += "\n" + day.title + ":\n"
 
-            for lesson in day.shedule:
+            for lesson in day.schedule:
                 spacer, temp_cab = 0, ''
-                
+
                 if len(lesson.cab) < 3:
                     temp_cab = "000"
                 elif len(lesson.cab) > 3:
                     spacer = 4
 
                 try:
-                    res += lesson.bell + " - " + temp_cab + lesson.cab + ' ' * (16 - len(lesson.cab) - len(temp_cab) - spacer) + str(lesson) + '\n'
+                    res += lesson.bell + " - " + temp_cab + lesson.cab + ' ' * \
+                        (16 - len(lesson.cab) - len(temp_cab) -
+                         spacer) + str(lesson) + '\n'
                 except:
                     pass
 
@@ -83,23 +132,27 @@ async def shedule(msg : types.Message, state : FSMContext):
         res += '\nВаши группы:\n'
 
         for i in range(2, len(groups), 3):
-            if (groups[i - 2]): res += (groups[i - 2] + ' ')
-            if (groups[i - 1]): res += (groups[i - 1] + ' ')
-            if (groups[i]): res += (groups[i])
+            if (groups[i - 2]):
+                res += (groups[i - 2] + ' ')
+            if (groups[i - 1]):
+                res += (groups[i - 1] + ' ')
+            if (groups[i]):
+                res += (groups[i])
 
             res += '\n'
 
-        LogPython.info(f"{msg.from_user.full_name} called {sys._getframe().f_code.co_name} [{msg.text}]")
-                
-        await bot.send_message(msg.from_user.id, res, parse_mode = 'markdown')
-    except Exception as e: 
+        LogPython.info(
+            f"{msg.from_user.full_name} called {sys._getframe().f_code.co_name} [{msg.text}]")
+
+        await bot.send_message(msg.from_user.id, res, parse_mode='markdown')
+    except Exception as e:
         import traceback
-        await bot.send_message(msg.from_user.id, "Unknown Identity. Try again (bye)")   
+        await bot.send_message(msg.from_user.id, "Unknown Identity. Try again (bye)")
         LogPython.error(e)
         LogPython.error(traceback.format_exc())
-        
+
     await state.finish()
-    
+
 if __name__ == '__main__':
     LogPython.info("Success connected")
-    executor.start_polling(dp, skip_updates = True)
+    executor.start_polling(dp, skip_updates=True)
